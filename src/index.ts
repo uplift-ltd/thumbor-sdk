03ec8727d86d0c11c543a7cf1ddd86f90f589d66
Marius Craciunoiu <marius@uplift.ltd>
2023-03-17 15:52:16 -0600

    Export UnsafeThumbor and option types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 export { Thumbor } from "./thubmor";
 
+export type { ThumborOptions } from "./thubmor";
+
+export { UnsafeThumbor } from "./thubmor";
+
+export type { UnsafeThumborOptions } from "./thubmor";
+
 export { buildUrl } from "./url";
 
 export type { BuildUrlOptions } from "./url";
